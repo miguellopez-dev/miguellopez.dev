d6e6d53f5782ca2a7ac761e8ec18d2d7e488fef8
Miguel Lopez <mlopezt.1215@gmail.com>
2024-01-20 07:27:27 -0800

    Add Content and styles

--- app/page.tsx
+++ app/page.tsx
@@ -1,12 +1,18 @@
+import { HiOutlineDocumentText } from 'react-icons/hi2';
+import { SlSocialLinkedin } from 'react-icons/sl';
+import { FiGithub } from 'react-icons/fi';
+import { MdAlternateEmail } from 'react-icons/md';
+import { IconContext } from 'react-icons';
+
 export default function HomePage() {
     return (
-        <div className='flex min-h-screen flex-col items-center justify-between p-24'>
-            <div>
-                <h1>Hi, I'm Miguel Lopez</h1>
-                <h2>Full Stack Developer</h2>
+        <div className='flex min-h-screen flex-col items-center space-y-20 p-24'>
+            <div className='flex flex-col items-center  space-y-6'>
+                <h1 className='text-6xl'>Hi, I'm Miguel Lopez</h1>
+                <h2 className='text-4xl'>Full Stack Developer</h2>
             </div>
-            <div>
-                <p>
+            <div className='mx-72'>
+                <p className='text-justify'>
                     As a proficient developer with expertise in React, Python,
                     and WordPress, I bring a blend of aesthetic design and
                     technical skill to every project. My passion for crafting
@@ -17,10 +23,18 @@ export default function HomePage() {
                     ideas into digital realities.
                 </p>
             </div>
-            <div>
-                <p>Social icons here</p>
+            <div className='flex space-x-4 text-3xl'>
+                <HiOutlineDocumentText />
+
+                <SlSocialLinkedin />
+                <FiGithub />
+                <MdAlternateEmail />
             </div>
-            <div>BUrron here</div>
+            <div>
+                <button className='bg-green-500 hover:bg-green-700 text-white font-bold py-2 px-4 rounded'>
+                    Send me a message
+                </button>
+            </div>
         </div>
     );
 }
