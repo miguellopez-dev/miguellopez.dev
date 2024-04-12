e3d5e2a0d8930018f1920d75648ddc088d618703
Miguel Lopez <mlopezt.1215@gmail.com>
2024-04-12 11:05:50 -0700

    Add animation when navigation to new page

--- app/page.tsx
+++ app/page.tsx
@@ -1,47 +1,62 @@
-import { HiOutlineDocumentText } from 'react-icons/hi2';
-import { SlSocialLinkedin } from 'react-icons/sl';
-import { FiGithub } from 'react-icons/fi';
-import { MdAlternateEmail } from 'react-icons/md';
+'use client';
 import { SiMinutemailer } from 'react-icons/si';
 import Image from 'next/image';
 import Socials from '@/components/Socials';
+import { motion } from 'framer-motion';
+import {
+    pageVariants,
+    pageTransition,
+} from '@/components/animations/pageAnimations';
 
 export default function HomePage() {
     return (
-        <div className='flex min-h-screen flex-col items-center space-y-10  py-10 relative'>
-            <div className='absolute right-0 top-0 -z-10'>
-                <Image
-                    src='/images/wolf-head-bg-large.png'
-                    alt='Wolf Head'
-                    width={300}
-                    height={400}
-                    sizes='(max-width: 1128px) 20vw,'
-                />
-            </div>
-            <div className='flex flex-col items-center  space-y-6'>
-                <h1 className='text-4xl lg:text-7xl'>Hi, I'm Miguel Lopez</h1>
-                <h2 className='text-2xl lg:text-5xl'>Full Stack Developer</h2>
-            </div>
-            <div className='w-10/12 md:w-2/3 lg:w-3/5 xl:w-1/2'>
-                <p className='text-justify text-l lg:text-2xl'>
-                    As a proficient developer with expertise in React, Python,
-                    and WordPress, I bring a blend of aesthetic design and
-                    technical skill to every project. My passion for crafting
-                    seamless user experiences and robust applications is evident
-                    in my work, ranging from dynamic appointment scheduling apps
-                    to engaging administrative dashboards. Let's build something
-                    amazing together. Explore my portfolio to see how I turn
-                    ideas into digital realities.
-                </p>
-            </div>
+        <motion.div
+            initial='initial'
+            animate='in'
+            exit='out'
+            variants={pageVariants}
+            transition={pageTransition}
+        >
+            <main className='flex min-h-screen flex-col items-center space-y-10  py-10 relative '>
+                <div className='absolute right-0 top-0 -z-10'>
+                    <Image
+                        src='/images/wolf-head-bg-large.png'
+                        alt='Wolf Head'
+                        width={300}
+                        height={400}
+                        sizes='(max-width: 1128px) 20vw,'
+                    />
+                </div>
+                <div className='flex flex-col items-center  space-y-6'>
+                    <h1 className='text-4xl lg:text-7xl'>
+                        Hi, I'm Miguel Lopez
+                    </h1>
+                    <h2 className='text-2xl lg:text-5xl'>
+                        Full Stack Developer
+                    </h2>
+                </div>
+                <div className='w-10/12 md:w-2/3 lg:w-3/5 xl:w-1/2'>
+                    <p className='text-justify text-l lg:text-2xl'>
+                        As a proficient developer with expertise in React,
+                        Python, and WordPress, I bring a blend of aesthetic
+                        design and technical skill to every project. My passion
+                        for crafting seamless user experiences and robust
+                        applications is evident in my work, ranging from dynamic
+                        appointment scheduling apps to engaging administrative
+                        dashboards. Let's build something amazing together.
+                        Explore my portfolio to see how I turn ideas into
+                        digital realities.
+                    </p>
+                </div>
 
-            <Socials />
-            <div>
-                <button className='bg-ctaGreen text-darkBlue flex items-center text-xl justify-center font-bold py-4 px-5 rounded hover:bg-darkBlue hover:text-mediumPurple hover:border hover:border-mediumPurple transition-colors duration-300'>
-                    <SiMinutemailer className='text-2xl mr-2' />
-                    Send me a message
-                </button>
-            </div>
-        </div>
+                <Socials />
+                <div>
+                    <button className='bg-ctaGreen text-darkBlue flex items-center text-xl justify-center font-bold py-4 px-5 rounded hover:bg-darkBlue hover:text-mediumPurple hover:border hover:border-mediumPurple transition-colors duration-300'>
+                        <SiMinutemailer className='text-2xl mr-2' />
+                        Send me a message
+                    </button>
+                </div>
+            </main>
+        </motion.div>
     );
 }
--- app/projects/page.tsx
+++ app/projects/page.tsx
@@ -1,22 +1,36 @@
+'use client';
 import Card from '@/components/Card';
+import { motion } from 'framer-motion';
+import {
+    pageVariants,
+    pageTransition,
+} from '@/components/animations/pageAnimations';
 
 export default function ProjectsPage() {
     return (
-        <main className='w-3/4 m-auto my-4 '>
-            <h1 className='text-center text-4xl'>Projects</h1>
-            <div className='my-4 p-4 space-y-6 flex flex-wrap  align-center justify-center lg:grid lg:grid-flow-col lg:gap-10'>
-                <Card title='Project 1' description='A project i made' />
-                <Card
-                    className='lg:mt-6'
-                    title='Project 2'
-                    description='Second Project?'
-                />
-                <Card
-                    className='lg:mt-14'
-                    title='Project 3'
-                    description='third Project?'
-                />
-            </div>
-        </main>
+        <motion.div
+            initial='initial'
+            animate='in'
+            exit='out'
+            variants={pageVariants}
+            transition={pageTransition}
+        >
+            <main className='w-3/4 m-auto my-4 '>
+                <h1 className='text-center text-4xl'>Projects</h1>
+                <div className='my-4 p-4 space-y-6 flex flex-wrap  align-center justify-center lg:grid lg:grid-flow-col lg:gap-10'>
+                    <Card title='Project 1' description='A project i made' />
+                    <Card
+                        className='lg:mt-6'
+                        title='Project 2'
+                        description='Second Project?'
+                    />
+                    <Card
+                        className='lg:mt-14'
+                        title='Project 3'
+                        description='third Project?'
+                    />
+                </div>
+            </main>
+        </motion.div>
     );
 }
