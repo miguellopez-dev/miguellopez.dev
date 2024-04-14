bd1d6f3859215adcb6dcf9d07a06308bd331c725
Miguel Lopez <mlopezt.1215@gmail.com>
2024-04-14 07:46:04 -0700

    Change grid layout for sm vs lg screen

--- app/technology/page.tsx
+++ app/technology/page.tsx
@@ -28,8 +28,8 @@ export default function TechonolgyPage() {
             transition={pageTransition}
         >
             <main className='flex min-h-screen flex-col items-center space-y-10  py-10 relative w-10/12 m-auto '>
-                <h1 className='uppercase'>Technologies Used</h1>
-                <div className='grid grid-cols-3 gap-12 lg:gap-20 text-9xl'>
+                <h1 className='text-4xl'>Technologies Used</h1>
+                <div className='grid grid-cols-2 lg:grid-cols-3 gap-12 lg:gap-20 text-9xl'>
                     <TbBrandTypescript />
                     <TbBrandJavascript />
                     <TbBrandTailwind />
