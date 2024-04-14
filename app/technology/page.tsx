da1699f1f9120c077f606c0e8c265849afe53b62
Miguel Lopez <mlopezt.1215@gmail.com>
2024-04-13 19:58:57 -0700

    Add tech used to tech page

--- app/technology/page.tsx
+++ app/technology/page.tsx
@@ -1,7 +1,47 @@
+'use client';
+import { motion } from 'framer-motion';
+import {
+    pageVariants,
+    pageTransition,
+} from '@/components/animations/pageAnimations';
+
+import {
+    TbBrandTypescript,
+    TbBrandReact,
+    TbBrandNextjs,
+    TbBrandJavascript,
+    TbBrandTailwind,
+    TbBrandHtml5,
+    TbBrandCss3,
+    TbBrandWordpress,
+    TbBrandMysql,
+    TbBrandGit,
+} from 'react-icons/tb';
+
 export default function TechonolgyPage() {
     return (
-        <main>
-            <h1>Technologies I Use</h1>
-        </main>
+        <motion.div
+            initial='initial'
+            animate='in'
+            exit='out'
+            variants={pageVariants}
+            transition={pageTransition}
+        >
+            <main className='flex min-h-screen flex-col items-center space-y-10  py-10 relative w-10/12 m-auto '>
+                <h1 className='uppercase'>Technologies Used</h1>
+                <div className='grid grid-cols-3 gap-12 lg:gap-20 text-9xl'>
+                    <TbBrandTypescript />
+                    <TbBrandJavascript />
+                    <TbBrandTailwind />
+                    <TbBrandReact />
+                    <TbBrandNextjs />
+                    <TbBrandHtml5 />
+                    <TbBrandCss3 />
+                    <TbBrandWordpress />
+                    <TbBrandMysql />
+                    <TbBrandGit />
+                </div>
+            </main>
+        </motion.div>
     );
 }
