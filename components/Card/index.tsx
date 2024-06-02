4b275633ea76dd8743148fd14ba3b0d57d2769c2
Miguel Lopez <mlopezt.1215@gmail.com>
2024-06-02 16:14:29 -0700

    Updated projected cards with live and github

--- components/Card/index.tsx
+++ components/Card/index.tsx
@@ -1,15 +1,28 @@
+import { SlGlobe } from 'react-icons/sl';
+import { TbBrandGit } from 'react-icons/tb';
+
 interface Props {
     title: string;
     description: string;
     imageUrl?: string;
+    liveUrl?: string;
+    githubUrl?: string;
     className?: string;
     style?: React.CSSProperties;
 }
 
-const Card = ({ title, description, imageUrl, className, style }: Props) => {
+const Card = ({
+    title,
+    description,
+    imageUrl,
+    liveUrl,
+    githubUrl,
+    className,
+    style,
+}: Props) => {
     return (
         <div
-            className={`bg-white shadow-md border border-gray-200 rounded-lg max-w-sm lg:h-min w-96 shadow-mediumPurple hover:shadow-lg hover:shadow-mediumPurple ${className}`}
+            className={`divide-y divide-altBlue bg-white shadow-md border border-gray-200 rounded-lg max-w-sm lg:h-min w-96 shadow-mediumPurple hover:shadow-lg hover:shadow-mediumPurple ${className}`}
             style={style}
         >
             {imageUrl && (
@@ -20,8 +33,30 @@ const Card = ({ title, description, imageUrl, className, style }: Props) => {
                 />
             )}
             <div className='px-6 py-4'>
-                <div className='font-bold text-xl mb-2'>{title}</div>
+                <div className='font-bold text-xl mb-2 text-altBlue'>
+                    {title}
+                </div>
                 <p className='text-gray-700 text-base'>{description}</p>
+                <div className='flex text-altBlue text-3xl mt-2 space-x-4'>
+                    {githubUrl ? (
+                        <a
+                            href={githubUrl}
+                            target='_blank'
+                            className='hover:text-mediumPurple'
+                        >
+                            <TbBrandGit />
+                        </a>
+                    ) : (
+                        <></>
+                    )}
+                    <a
+                        href={liveUrl}
+                        target='_blank'
+                        className='hover:text-mediumPurple'
+                    >
+                        <SlGlobe />
+                    </a>
+                </div>
             </div>
             <div className='px-6 pt-4 pb-2'>
                 <span className='inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2 mb-2'>
